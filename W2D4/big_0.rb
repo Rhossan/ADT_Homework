ae425bd27322e3925233af5ce2fb443e63e11386
Rhossan <rhossan93@gmail.com>
2018-03-29 00:27:00 -0400

    adds different time complexities for searching an array

--- W2D4/big_0.rb
+++ W2D4/big_0.rb
@@ -1,3 +1,5 @@
+
+
 def sluggish_octopus(fishes)
   # max_length = fishes[0].length
   for i in 0..fishes.length-2
@@ -11,4 +13,53 @@ def sluggish_octopus(fishes)
 
 end
 
-sluggish_octopus(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
+p sluggish_octopus(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
+
+class Array
+  def merge_sort(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    return self if count <= 1
+
+    midpoint = count / 2
+    sorted_left = self.take(midpoint).merge_sort(&prc)
+    sorted_right = self.drop(midpoint).merge_sort(&prc)
+
+    Array.merge(sorted_left, sorted_right, &prc)
+  end
+
+  def self.merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        merged << left.shift
+      when 0
+        merged << left.shift
+      when 1
+        merged << right.shift
+      end
+    end
+
+    merged.concat(left)
+    merged.concat(right)
+    merged
+  end
+end
+
+def dominant_octopus
+  ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'].merge_sort
+end
+
+def clever_octopus(arr)
+  arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+  max = arr[0]
+  arr.each_index do |idx|
+    if el.length > arr.length
+      max = arr[idx]
+    end
+  end
+  max
+end
+
+fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
